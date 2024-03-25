51cb8c97f8e5a054b5235a5b9a63e75ade7bb788
wwj <wwj@wwjdeMacBook-Air.local>
2024-03-25 13:55:15 +0800

    scope improve

--- Example/Common/ViewController.swift
+++ Example/Common/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Common
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let star = "ddgdsdada"
+        if star.isValidPhoneNumber {
+            print("")
+        }
     }
 
     override func didReceiveMemoryWarning() {
